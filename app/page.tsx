0865d68b8388db1ee3ccfa1651d9aead24eba730
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2025-02-04 14:16:45 -0300

    add dummy content to render

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 export default function Home() {
   return (
     <div>
-
+      <h1 className="bg-white text-black">Home</h1>
+      <h1 className="bg-white text-black">Olá</h1>
     </div>
   );
 }
