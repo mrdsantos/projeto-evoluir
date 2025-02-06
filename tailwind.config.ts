f3de4c44a0c26599b8021165be6c0f70fc1cef3c
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2025-02-06 04:01:23 -0300

    add daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,23 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
-export default {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["emerald", "dim"],
+    darkTheme: "dim",
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "daisy-",
+    logs: true,
+    themeRoot: ":root",
   },
-  plugins: [],
-} satisfies Config;
+};
+
+export default config;
