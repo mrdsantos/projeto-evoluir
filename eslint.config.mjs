743cec268d8991be85239e1118ad719d298795ed
Maicon R. dos Santos <maiconrdsantos88@gmail.com>
2025-02-05 22:54:48 -0300

    update rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,23 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+	baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  ...compat.config({
-    extends: ["next"],
-    rules: {
-      "no-unused-vars": "off",
-    },
-  }),
-];
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.config({
+		extends: ["next", "prettier"],
+		rules: {
+			"no-unused-vars": "off", // Desativa regra padrão do ESLint
+			"@typescript-eslint/no-unused-vars": ["error"], // Usa a versão do TypeScript para evitar variáveis não utilizadas
+		},
+	}),
+]
 
-export default eslintConfig;
+export default eslintConfig
